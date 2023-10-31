84193de90abbe1ae012128af7d8efe2a9daa51ea
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-10-31 20:20:19 +0200

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
+/* test test */
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
