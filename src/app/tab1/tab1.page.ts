7742040f9a2adbcd614012feb5e06b4691b62614
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-10-31 20:22:08 +0200

    test

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   constructor() {}
-
+  // test
 }
