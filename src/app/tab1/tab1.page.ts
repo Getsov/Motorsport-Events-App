aedfa2d1993fa33d5c9a984d17874e6363ac0ce9
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-01 20:16:41 +0200

    set homePage ui with hardcode data

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,4 +9,42 @@ export class Tab1Page {
 
   constructor() {}
 
+<<<<<<< Updated upstream
+=======
+  items: any[] = [
+    {
+      type: 'All',
+      imageUrl: 'https://m.media-amazon.com/images/I/41i2wgm-JsL.jpg',
+    },
+    {
+      type: 'Off road',
+      imageUrl:
+        'https://www.rudaw.net/Library/Assets//Gallery/Photos/Photos2020/1April-1june/BlindTahir/peshbrke-09092020/peshbrki-09092020%20(10).JPG',
+    },
+    {
+      type: 'Drag racing',
+      imageUrl: 'https://i.ytimg.com/vi/KpPni9A0jfY/maxresdefault.jpg',
+    },
+    {
+      type: 'Drift',
+      imageUrl:
+        'https://gatebil.no/wp-content/uploads/2023/04/r6_l4692-scaled.jpg',
+    },
+    {
+      type: 'Rally',
+      imageUrl:
+        'https://www.firestone.com.mx/content/dam/consumer/fst/la/mx/tips/otros/rally_b.jpg',
+    },
+    {
+      type: 'Festival',
+      imageUrl:
+        'https://i0.wp.com/coloradocarshow.com/wp-content/uploads/2023/06/welcome.jpg?fit=1000%2C667&ssl=1',
+    },
+    {
+      type: 'Carting race',
+      imageUrl:
+        'https://curiocity.com/wp-content/uploads/2022/05/BeFunky-collage-44.jpg',
+    },
+  ];
+>>>>>>> Stashed changes
 }
