a087401aa636f09eeda199315ab8df5108d4d4d2
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2023-11-02 15:14:14 +0200

    Change to tabs router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,27 +5,7 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
-    path: 'calendar',
-    loadChildren: () => import('./calendar/calendar.module').then( m => m.CalendarPageModule)
   },
-  {
-    path: 'profile',
-    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
-  },
-  {
-    path: 'notifications',
-    loadChildren: () => import('./notifications/notifications.module').then( m => m.NotificationsPageModule)
-  },
-  {
-    path: 'events',
-    loadChildren: () => import('./events/events.module').then( m => m.EventsPageModule)
-  },
-
-  /* {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  } */
 
 ];
 @NgModule({
