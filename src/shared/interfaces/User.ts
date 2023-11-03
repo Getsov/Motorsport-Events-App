32271b935dea2add63a5e559595b9f2b0688b7a4
AntonovAtanas <atanas.antonov1@gmail.com>
2023-11-03 09:20:19 +0200

    Add additional properties to User interface

--- src/shared/interfaces/User.ts
+++ src/shared/interfaces/User.ts
@@ -1,6 +1,10 @@
 export interface User {
-    username: string;
     email: string;
-    password: string;
+    role: string; 
+    firstName?: string; 
+    lastName?: string; 
+    city: string;
+    isDeleted: boolean;
+    likedEvents: { id: string }[];
     _id?: string;
 }
