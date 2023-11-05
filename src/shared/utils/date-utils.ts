05c21f50c0b812638c2821ec27e03a52f2f00274
AntonovAtanas <atanas.antonov1@gmail.com>
2023-11-05 20:43:19 +0200

    Add util function to get the day of the week

--- src/shared/utils/date-utils.ts
+++ src/shared/utils/date-utils.ts
@@ -1,20 +1,23 @@
 export function getDayOfWeek(dateString: string): string {
   const daysOfWeek = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
+    'Неделя',
+    'Понеделник',
+    'Вторник',
+    'Сряда',
+    'Четвъртък',
+    'Петък',
+    'Събота',
   ];
 
-  const date = new Date(dateString);
+    const parts = dateString.split('.');
+    const compatibleDateString = `${parts[2]}-${parts[1]}-${parts[0]}`;
 
-  if (!isNaN(date.getTime())) {
-    const dayIndex = date.getDay();
-    return daysOfWeek[dayIndex];
-  } else {
-    return 'Invalid Date';
-  }
+    const date = new Date(compatibleDateString);
+
+    if (!isNaN(date.getTime())) {
+      const dayIndex = date.getDay();
+      return daysOfWeek[dayIndex];
+    } else {
+      return 'Invalid Date';
+    }
 }
