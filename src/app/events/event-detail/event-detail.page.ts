0c2bbed13baeaff28b5c245a63a71452713631fc
AntonovAtanas <atanas.antonov1@gmail.com>
2023-11-05 07:43:24 +0200

    Create initial ionic page

---
+++ src/app/events/event-detail/event-detail.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-detail',
+  templateUrl: './event-detail.page.html',
+  styleUrls: ['./event-detail.page.scss'],
+})
+export class EventDetailPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('ttest')
+  }
+
+}
