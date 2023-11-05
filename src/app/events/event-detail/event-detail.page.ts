9729d68dae119ebf6ddf23eb6e5c111c75c691c7
AntonovAtanas <atanas.antonov1@gmail.com>
2023-11-05 08:32:08 +0200

    Add mock event data

--- src/app/events/event-detail/event-detail.page.ts
+++ src/app/events/event-detail/event-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Event } from 'src/shared/interfaces/Event';
 
 @Component({
   selector: 'app-event-detail',
@@ -6,11 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-detail.page.scss'],
 })
 export class EventDetailPage implements OnInit {
+  mockEvent: Event = {
+    title: 'Драг Рейсинг на Кондофрей',
+    imageUrl:
+      'https://scontent.fsof8-1.fna.fbcdn.net/v/t39.30808-6/336647635_186029427479392_3389018506494843726_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=LHyD876d-rQAX-SWQ9P&_nc_ht=scontent.fsof8-1.fna&oh=00_AfAEeMnhiiEjYIDFUtzU3HZtFuUM91SO06_xGj1cC45yuw&oe=654C5316',
+    shortDescription: 'Драг Рейсинг - Кондофрей [29-30 Април]',
+    longDescription: `Събитието ще се проведе на познатата ни писта на летище Sofia West Airport, с. Кондофрей до гр. Радомир.
+    Вход: 20лв - Такса свободни стартове: 50лв - Такса участие: 60лв`,
+    dates: [{date: '29 Apr 2023', startTime: '9AM', endTime: '7:30PM'}, {date: '30 Apr 2023', startTime: '9AM', endTime: '7:30PM'}],
+    contacts: {
+      city: 'Кондофрей',
+      address: 'Sofia West Airport',
+      phone: '0888888888',
+      email: 'kondofrey@abv.bg',
+      coordinates: {lat: '42.448154', long: '22.963561'},
+    },
+    category: 'Драг',
+    creator: 'Drag Racing Bulgaria',
+    id: '01',
+    isDeleted: false,
+    likedCount: 2
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-    
-  }
-
+  ngOnInit() {}
 }
