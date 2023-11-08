2298ef27433c2fe32c0885d3fc21404c9bb83b84
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-08 22:57:50 +0200

    create shared module and move the like icon component there

---
+++ src/app/shared-module/like-icon.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { HomePageModule } from 'src/app/home/home.module';
+
+@Component({
+  selector: 'app-like-icon',
+  templateUrl: './like-icon.component.html',
+  styleUrls: ['./like-icon.component.scss'],
+})
+export class LikeIconComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/shared-module/shared-module.module.ts
@@ -0,0 +1,19 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { LikeIconComponent } from './like-icon.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ExploreContainerComponentModule,
+    FormsModule,
+    IonicModule,
+  ],
+  declarations: [LikeIconComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  exports: [LikeIconComponent],
+})
+export class SharedModule {}
