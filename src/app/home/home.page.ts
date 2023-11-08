6308a927b389eed70edf1f006253019802d50dd8
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-08 11:11:12 +0200

    finalize little details on designer request

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,23 +15,21 @@ export class HomePage implements OnInit {
     // TODO : fetch lastest events data
     this.upcomingEvents = [
       {
-        banner:
-          'https://images.squarespace-cdn.com/content/v1/5a8227a58fd4d2d73f0e95c4/1550018105210-0RDB6YCPR7IJ6RNRJVGA/Drift-Rides-030.png?format=1000w',
+        banner: '../../assets/icon/Image.svg',
         _id: 'asdasdsadsa',
         shortTitle: 'Кондофрей Драг 2023',
         startDate: '12-11-2023',
         endDate: '14.11.23',
       },
       {
-        banner: 'https://superlift.com/images/Blog/ORE_Image2.jpg',
+        banner: '../../assets/icon/Image.svg',
         _id: 'asdasdas',
         shortTitle: 'Кондофрей Драг 2023',
         startDate: '12-11-2023',
         endDate: '14.11.23',
       },
       {
-        banner:
-          'https://www.timeattack.co.uk/wp-content/uploads/2021/03/TA2018r3_Brands-Hatch-Pitlane-web.jpg',
+        banner: '../../assets/icon/Image.svg',
         _id: 'asdasdasd',
         shortTitle: 'Кондофрей Драг 2023',
         startDate: '12-11-2023',
