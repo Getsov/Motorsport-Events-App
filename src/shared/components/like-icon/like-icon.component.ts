785a0ba1d8321d76690bdf4b1c9257e14d8ea94b
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-13 22:39:44 +0200

    complete like icon functionality
    1. Dynamic color on like-unlike (font and background).
    2. Dynamic size of icon then digits length change.
    3. Dynamic positioning of likedCount paragraph based on icon size.
    4. Set the dynamic properties On init with the passed data.

--- src/shared/components/like-icon/like-icon.component.ts
+++ src/shared/components/like-icon/like-icon.component.ts
@@ -16,38 +16,68 @@ export class LikeIconComponent implements OnInit {
   isLiked: boolean = false;
   fontSize: string = '10px';
 
-  ngOnInit() {}
+  ngOnInit() {
+    // Check if is liked and set the boolean.
+    this.isLiked = false;
+
+    // configurate icon
+    if (this.likedCount < 10) {
+      this.fontSize = '10px';
+
+      this.isLiked
+        ? (this.likeIconSrc = '../../../assets/icon/like-icons/small-liked.png')
+        : (this.likeIconSrc =
+            '../../../assets/icon/like-icons/not-liked-small.png');
+    } else if (this.likedCount < 100) {
+      this.fontSize = '12px';
+
+      this.isLiked
+        ? (this.likeIconSrc =
+            '../../../assets/icon/like-icons/medium-liked.png')
+        : (this.likeIconSrc =
+            '../../../assets/icon/like-icons/not-liked-medium.png');
+    } else if (this.likedCount < 1000) {
+      this.fontSize = '14px';
+
+      this.isLiked
+        ? (this.likeIconSrc = '../../../assets/icon/like-icons/large-liked.png')
+        : (this.likeIconSrc =
+            '../../../assets/icon/like-icons/not-liked-large.png');
+    }
+  }
 
   likeEvent() {
     this.isLiked = !this.isLiked;
     this.likedCount++;
 
     if (this.likedCount < 10) {
-      this.likeIconSrc =
-        this.likeIconSrc ===
-        '../../../assets/icon/like-icons/not-liked-small.png'
-          ? '../../../assets/icon/like-icons/small-liked.png'
-          : '../../../assets/icon/like-icons/not-liked-small.png';
+      this.isLiked;
+      '../../../assets/icon/like-icons/not-liked-small.png'
+        ? (this.likeIconSrc = '../../../assets/icon/like-icons/small-liked.png')
+        : (this.likeIconSrc =
+            '../../../assets/icon/like-icons/not-liked-small.png');
     } else if (this.likedCount < 100) {
       if (this.likedCount === 10) {
         this.fontSize = '12px';
-        console.log('fontsizre changed');
       }
-      this.likeIconSrc =
-        this.likeIconSrc ===
-        '../../../assets/icon/like-icons/not-liked-medium.png'
-          ? '../../../assets/icon/like-icons/medium-liked.png'
-          : '../../../assets/icon/like-icons/not-liked-medium.png';
+
+      this.isLiked
+        ? (this.likeIconSrc =
+            '../../../assets/icon/like-icons/medium-liked.png')
+        : (this.likeIconSrc =
+            '../../../assets/icon/like-icons/not-liked-medium.png');
     } else if (this.likedCount < 1000) {
-      this.likeIconSrc =
-        this.likeIconSrc ===
-        '../../../assets/icon/like-icons/not-liked-large.png'
-          ? '../../../assets/icon/like-icons/large-liked.png'
-          : '../../../assets/icon/like-icons/not-liked-large.png';
+      if (this.likedCount === 10) {
+        this.fontSize = '14px';
+      }
+
+      this.isLiked
+        ? (this.likeIconSrc = '../../../assets/icon/like-icons/large-liked.png')
+        : (this.likeIconSrc =
+            '../../../assets/icon/like-icons/not-liked-large.png');
     }
 
     // Send a request to like the event using the eventId
-    // if is liked unlike it, if is not liked - like it.
 
     console.log(`Liked event with ID: ${this.eventId}`);
   }
