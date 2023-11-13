ac80a09620753f1d8a77dc0df578db8932db744f
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-13 20:43:23 +0200

    add like icon functionality and change color on like

--- src/shared/components/like-icon/like-icon.component.ts
+++ src/shared/components/like-icon/like-icon.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+// like-icon.component.ts
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-like-icon',
@@ -8,5 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class LikeIconComponent implements OnInit {
   constructor() {}
 
+  @Input() eventId: string = '';
+  @Input() likesCount: number = 0;
+
+  likeIconSrc: string = '../../../assets/icon/like-icons/not-liked-small.png';
+  isLiked: boolean = false;
   ngOnInit() {}
+
+  likeEvent() {
+    this.likeIconSrc =
+      this.likeIconSrc === '../../../assets/icon/like-icons/not-liked-small.png'
+        ? '../../../assets/icon/like-icons/small-liked.png'
+        : '../../../assets/icon/like-icons/not-liked-small.png';
+
+    this.isLiked = !this.isLiked;
+
+    // Send a request to like the event using the eventId
+    // if is liked unlike it, if is not liked - like it.
+
+    console.log(`Liked event with ID: ${this.eventId}`);
+  }
 }
