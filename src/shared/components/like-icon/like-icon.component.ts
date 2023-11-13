a4180a4d0516092966cbc4f68998215758582189
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-13 21:18:56 +0200

    Change icon size, color, font-size then needed

--- src/shared/components/like-icon/like-icon.component.ts
+++ src/shared/components/like-icon/like-icon.component.ts
@@ -14,15 +14,37 @@ export class LikeIconComponent implements OnInit {
 
   likeIconSrc: string = '../../../assets/icon/like-icons/not-liked-small.png';
   isLiked: boolean = false;
+  fontSize: string = '10px';
+
   ngOnInit() {}
 
   likeEvent() {
-    this.likeIconSrc =
-      this.likeIconSrc === '../../../assets/icon/like-icons/not-liked-small.png'
-        ? '../../../assets/icon/like-icons/small-liked.png'
-        : '../../../assets/icon/like-icons/not-liked-small.png';
-
     this.isLiked = !this.isLiked;
+    this.likesCount++;
+
+    if (this.likesCount < 10) {
+      this.likeIconSrc =
+        this.likeIconSrc ===
+        '../../../assets/icon/like-icons/not-liked-small.png'
+          ? '../../../assets/icon/like-icons/small-liked.png'
+          : '../../../assets/icon/like-icons/not-liked-small.png';
+    } else if (this.likesCount < 100) {
+      if (this.likesCount === 10) {
+        this.fontSize = '12px';
+        console.log('fontsizre changed');
+      }
+      this.likeIconSrc =
+        this.likeIconSrc ===
+        '../../../assets/icon/like-icons/not-liked-medium.png'
+          ? '../../../assets/icon/like-icons/medium-liked.png'
+          : '../../../assets/icon/like-icons/not-liked-medium.png';
+    } else if (this.likesCount < 1000) {
+      this.likeIconSrc =
+        this.likeIconSrc ===
+        '../../../assets/icon/like-icons/not-liked-large.png'
+          ? '../../../assets/icon/like-icons/large-liked.png'
+          : '../../../assets/icon/like-icons/not-liked-large.png';
+    }
 
     // Send a request to like the event using the eventId
     // if is liked unlike it, if is not liked - like it.
