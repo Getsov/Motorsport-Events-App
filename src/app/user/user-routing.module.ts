d5d8bc8c21ef242d1890a04bf4bee2e8d91d5bfe
AntonovAtanas <atanas.antonov1@gmail.com>
2023-11-14 14:09:35 +0200

    Added user module routing: user/profile, user/login

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    // TODO: Auth guard to check if user is logged in
-    // If yes - redirect to Profile page. If no - redirect to Login page.
+  // TODO: Auth guard to check if user is logged in
+  // If yes - redirect to Profile page. If no - redirect to Login page.
   {
-    path: '',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
-  }
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginPageModule),
+  },
+  {
+    path: 'profile',
+    loadChildren: () =>
+      import('./profile/profile.module').then((m) => m.ProfilePageModule),
+  },
 ];
 
 @NgModule({
