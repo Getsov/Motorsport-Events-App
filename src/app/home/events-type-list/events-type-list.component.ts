37d1391f104e60f20767e2cc892339f77c24bebb
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-14 23:21:46 +0200

    Pass the type of selected category in state, keep url clean.

--- src/app/home/events-type-list/events-type-list.component.ts
+++ src/app/home/events-type-list/events-type-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { Category } from 'src/shared/interfaces/Category';
 
 @Component({
   selector: 'app-events-type-list',
@@ -6,15 +8,25 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./events-type-list.component.scss'],
 })
 export class EventsTypeListComponent implements OnInit {
-  @Input() eventsType: any[] = [];
+  @Input() eventsType: Category[] = [];
 
   //  We set default values on titleText and titleColor.
   // If data is passed they will change its value.
   @Input() titleColor = 'yellow';
   @Input() titleText = 'Категории';
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   // TODO: redirect to eventsList and pass the selected filter(type of the event)
   ngOnInit() {}
+
+  redirectWithState(filter: string) {
+    let navigationExtras: NavigationExtras = {
+      state: {
+        filter,
+      },
+    };
+
+    this.router.navigate(['/tabs/events'], navigationExtras);
+  }
 }
