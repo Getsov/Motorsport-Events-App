0b59687f5dc9f88b3b7ade1760a400b9b70983a4
AntonovAtanas <atanas.antonov1@gmail.com>
2023-11-14 10:11:05 +0200

    Add page header title + ionic segment buttons

--- src/app/user/login/login.module.ts
+++ src/app/user/login/login.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { SharedModule } from 'src/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    SharedModule
   ],
   declarations: [LoginPage]
 })
