9b9ea7c6526b36dc6cb3a27c0b700a93742108cf
AntonovAtanas <atanas.antonov1@gmail.com>
2023-11-17 19:30:02 +0200

    Add user registration page
    - Create the page as per design
    - Add arrow svg icon for the dropdown
    
    -TODO: validation and logic

--- src/app/user/user-register/user-register.page.ts
+++ src/app/user/user-register/user-register.page.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import BulgarianRegions from 'src/shared/data/regions';
+
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.page.html',
   styleUrls: ['./user-register.page.scss'],
 })
 export class UserRegisterPage implements OnInit {
+  userChecked: boolean = true;
 
-  constructor() { }
+  bulgarianRegions: string[] = Object.keys(BulgarianRegions);
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
