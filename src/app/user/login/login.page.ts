0771ef4283607936d581b5af5511f7ccf8a4819e
AntonovAtanas <atanas.antonov1@gmail.com>
2023-11-17 19:39:37 +0200

    Add dynamically segment change from login to register
    - TODO the link "регистрирате от ТУК" transitions correctly but very hard. Not as smooth as with the segment

--- src/app/user/login/login.page.ts
+++ src/app/user/login/login.page.ts
@@ -6,15 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  backButton: boolean = false;
-
-  authSegment: string = 'login';
-
   constructor() {}
 
   ngOnInit() {}
-
-  segmentChanged(ev: any) {
-    this.authSegment = ev.detail.value;
-  }
 }
