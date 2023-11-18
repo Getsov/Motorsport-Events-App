f1c4699cd67be4c4cf856038a3ad1eea3e88aac3
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-11-18 18:47:22 +0200

    position and set size for zero nortifications view

--- src/shared/components/header/header.component.ts
+++ src/shared/components/header/header.component.ts
@@ -11,7 +11,7 @@ export class HeaderComponent implements OnInit {
   @Input() defaultHref: string = '';
 
   constructor() {}
-  notifications: number = 1;
+  notifications: number = 0;
   // TODO: fetch notifications
   // TODO: add conditional rendering for notifications number container
   ngOnInit() {}
