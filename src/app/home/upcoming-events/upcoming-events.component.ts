a3a2dce5d191e8bd4ea2a5a04e7884b4f1285fe3
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-12-01 22:03:48 +0200

    make to load more data then we are at the middle of the view

--- src/app/home/upcoming-events/upcoming-events.component.ts
+++ src/app/home/upcoming-events/upcoming-events.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { IonicSlides } from '@ionic/angular';
 import { Event } from 'src/shared/interfaces/Event';
 import { EventsService } from 'src/shared/services/events.service';
-import { register } from 'swiper/element/bundle';
-register();
 
 @Component({
   selector: 'app-upcoming-events',
@@ -12,22 +9,60 @@ register();
 })
 export class UpcomingEventsComponent implements OnInit {
   @Input() upcomingEvents: Event[] = [];
+  @Input() loadEventsCount: number = 5;
 
   @Input() titleText: string = 'Предстоящи събития';
   @Input() titleColor: string = 'orange';
 
-  swiperModule = [IonicSlides];
+  pageToLoad: number = 1;
+  isAllEventsLoaded: boolean = false;
 
   constructor(private eventService: EventsService) {}
 
   ngOnInit() {
-    this.eventService.getPaginationEvents(1, 5).subscribe({
-      next: (response) => {
-        console.log(response.results);
+    this.loadEvents();
+  }
 
-        this.upcomingEvents = response.results;
-      },
-      error: (error) => console.log(error),
-    });
+  private loadEvents() {
+    this.eventService
+      .getPaginationEvents(this.pageToLoad, this.loadEventsCount)
+      .subscribe({
+        next: (response) => {
+          this.upcomingEvents = response.results;
+          console.log(this.upcomingEvents);
+
+          console.log(this.pageToLoad);
+          this.pageToLoad++;
+        },
+        error: (error) => console.log(error),
+      });
+  }
+
+  swiperSlideChanged(e: any) {
+    let activeIndex = e.detail[0].activeIndex;
+
+    // Add a flag to track whether all events have been loaded
+    // Load more events 2 dots before the end.
+    if (
+      !this.isAllEventsLoaded &&
+      this.upcomingEvents.length - activeIndex == 3
+    ) {
+      this.eventService
+        .getPaginationEvents(this.pageToLoad, this.loadEventsCount)
+        .subscribe({
+          next: (response) => {
+            if (response.nextPage) {
+              this.upcomingEvents.push(...response.results);
+              console.log(this.upcomingEvents);
+              console.log(this.pageToLoad);
+              this.pageToLoad++;
+            } else {
+              // Set the flag if no more events are available
+              this.isAllEventsLoaded = true;
+            }
+          },
+          error: (error) => console.log(error),
+        });
+    }
   }
 }
