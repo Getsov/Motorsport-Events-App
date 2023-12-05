980db693e5c5aa5005a7515f9d48b25c8dad4bd1
AntonovAtanas <atanas.antonov1@gmail.com>
2023-12-05 12:30:18 +0200

    Add organization registration handler via authServices

--- src/app/user/organization-register/organization-register.page.ts
+++ src/app/user/organization-register/organization-register.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/shared/services/auth.service';
+
 import BulgarianRegions from 'src/shared/data/regions';
 
 @Component({
@@ -12,7 +14,10 @@ export class OrganizationRegisterPage implements OnInit {
     isNaN(Number(v))
   );
 
-  constructor() {}
+  authResponseError: string = '';
+  selectedRegion: string = '';
+
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {}
 
@@ -20,5 +25,34 @@ export class OrganizationRegisterPage implements OnInit {
     if (organizatorRegisterForm.invalid) {
       return;
     }
+
+    const { email, password, repass, organizatorName, phone } =
+      organizatorRegisterForm.value;
+
+    const role = 'organizer';
+    const region = this.selectedRegion;
+
+    this.authService
+      .registerOrganizator(
+        email,
+        password,
+        repass,
+        organizatorName,
+        phone,
+        region,
+        role
+      )
+      .subscribe({
+        next: (authResponse) => {
+          console.log(authResponse);
+        },
+        error: (error) => {
+          this.authResponseError = error.error.error;
+        },
+      });
+  }
+
+  onRegionChange(region: string) {
+    this.selectedRegion = region;
   }
 }
