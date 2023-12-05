e249bfbc1cd8d6bea57ac052a90d5d3183d90925
AntonovAtanas <atanas.antonov1@gmail.com>
2023-12-05 15:17:26 +0200

    Persist auth data in local storage
    - would it be better to use @capacitor/preferences?

--- src/shared/services/auth.service.ts
+++ src/shared/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { AuthResponseData } from '../interfaces/User';
+import { BehaviorSubject, tap } from 'rxjs';
 
 const { baseUrl } = environment;
 
@@ -9,6 +10,9 @@ const { baseUrl } = environment;
   providedIn: 'root',
 })
 export class AuthService {
+  private userDataSubject = new BehaviorSubject<AuthResponseData | null>(null);
+  userData$ = this.userDataSubject.asObservable();
+
   constructor(private http: HttpClient) {}
 
   registerUser(
@@ -19,14 +23,16 @@ export class AuthService {
     lastName: string,
     region: string
   ) {
-    return this.http.post<AuthResponseData>(`${baseUrl}/user/registerUser`, {
-      email,
-      password,
-      repass,
-      firstName,
-      lastName,
-      region,
-    });
+    return this.http
+      .post<AuthResponseData>(`${baseUrl}/user/registerUser`, {
+        email,
+        password,
+        repass,
+        firstName,
+        lastName,
+        region,
+      })
+      .pipe(tap((userData) => this.setUserData(userData)));
   }
 
   registerOrganizator(
@@ -38,21 +44,36 @@ export class AuthService {
     region: string,
     role: string
   ) {
-    return this.http.post<AuthResponseData>(`${baseUrl}/user/registerUser`, {
-      email,
-      password,
-      repass,
-      organizatorName,
-      phone,
-      region,
-      role,
-    });
+    return this.http
+      .post<AuthResponseData>(`${baseUrl}/user/registerUser`, {
+        email,
+        password,
+        repass,
+        organizatorName,
+        phone,
+        region,
+        role,
+      })
+      .pipe(tap((userData) => this.setUserData(userData)));
   }
 
   login(email: string, password: string) {
-    return this.http.post<AuthResponseData>(`${baseUrl}/user/loginUser`, {
-      email,
-      password,
-    });
+    return this.http
+      .post<AuthResponseData>(`${baseUrl}/user/loginUser`, {
+        email,
+        password,
+      })
+      .pipe(tap((userData) => this.setUserData(userData)));
+  }
+
+  // Store it in localStorage for persistence
+  setUserData(userData: AuthResponseData): void {
+    this.userDataSubject.next(userData);
+    localStorage.setItem('authData', JSON.stringify(userData));
+  }
+
+  logout(): void {
+    this.userDataSubject.next(null);
+    localStorage.removeItem('authData');
   }
 }
