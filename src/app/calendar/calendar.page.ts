829e0046b2f86a11ff74b414ebdc1a1fbd552cd4
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-12-10 15:10:08 +0200

    set page header with title and backbutton

--- src/app/calendar/calendar.page.ts
+++ src/app/calendar/calendar.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
@@ -7,6 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarPage implements OnInit {
   constructor() {}
-
+  @Input() pageTitle: string = 'Календар';
   ngOnInit() {}
 }
