2227af8cc48f875ba1b13bf57f3c9f2e42dd2c9d
AntonovAtanas <114076833+AntonovAtanas@users.noreply.github.com>
2023-12-12 10:13:33 +0200

    Add auth guard to Profile page

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from 'src/shared/guards/auth.guard';
+
 const routes: Routes = [
   // TODO: Auth guard to check if user is logged in
   // If yes - redirect to Profile page. If no - redirect to Login page.
@@ -13,6 +15,7 @@ const routes: Routes = [
     path: 'profile',
     loadChildren: () =>
       import('./profile/profile.module').then((m) => m.ProfilePageModule),
+    canLoad: [AuthGuard],
   },
 ];
 
