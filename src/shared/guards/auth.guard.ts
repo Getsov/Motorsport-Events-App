93333b9d514fff51135e231c09457eecd9d00317
AntonovAtanas <114076833+AntonovAtanas@users.noreply.github.com>
2023-12-12 10:13:00 +0200

    Create Auth guard

---
+++ src/shared/guards/auth.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanLoad {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (!this.authService.isAuthenticated()) {
+      this.router.navigateByUrl('tabs/user/auth');
+    }
+
+    return this.authService.isAuthenticated();
+  }
+}
