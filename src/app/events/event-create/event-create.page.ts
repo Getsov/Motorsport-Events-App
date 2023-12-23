f0a6a98422f1463097eb5bf027acdfe17205855e
AntonovAtanas <114076833+AntonovAtanas@users.noreply.github.com>
2023-12-23 22:55:24 +0200

    Add form submit functionality

--- src/app/events/event-create/event-create.page.ts
+++ src/app/events/event-create/event-create.page.ts
@@ -4,6 +4,9 @@ import { SelectPriceComponent } from './select-price/select-price.component';
 import { SelectDatesComponent } from './select-dates/select-dates.component';
 import BulgarianRegions from 'src/shared/data/regions';
 import Categories from 'src/shared/data/categories';
+import { AuthService } from 'src/shared/services/auth.service';
+import { Subscription } from 'rxjs';
+import { EventsService } from 'src/shared/services/events.service';
 
 @Component({
   selector: 'app-event-create',
@@ -11,6 +14,9 @@ import Categories from 'src/shared/data/categories';
   styleUrls: ['./event-create.page.scss'],
 })
 export class EventCreatePage implements OnInit {
+  eventSubscription$!: Subscription;
+  errorMessage: string = '';
+
   // get price values
   @ViewChild(SelectPriceComponent) selectPricesComponent!: SelectPriceComponent;
   visitorPrices = [{ price: '', description: '' }];
@@ -57,21 +63,63 @@ export class EventCreatePage implements OnInit {
   priceSeparatorColor: string = 'yellow';
   priceSeparatorTitle: string = 'Цени';
 
-  constructor() {}
+  constructor(
+    private authService: AuthService,
+    private eventService: EventsService
+  ) {}
 
   ngOnInit() {}
 
   onCreateEventSubmit(eventForm: NgForm) {
-    // TODO: validation
+    if (eventForm.invalid) {
+      return;
+    }
+
+    const user = this.authService.getUser();
+
+    if (!user) {
+      this.errorMessage = 'User not authernticated';
+      return;
+    }
 
     const formValue = {
-      ...eventForm.value,
+      shortTitle: eventForm.value.shortTitle,
+      ...(eventForm.value.longTitle && {
+        longTitle: eventForm.value.longTitle,
+      }),
+      shortDescription: eventForm.value.shortDescription,
+      ...(eventForm.value.longDescription && {
+        longDescription: eventForm.value.longDescription,
+      }),
+      imageUrl: this.imageUrl,
+      category: this.selectedEventType,
+      contacts: {
+        coordinates: { lat: 42.671227, long: 23.369354 }, // hard coded coordinates
+        region: this.selectedRegion,
+        address: eventForm.value.address,
+        email: eventForm.value.email,
+        phone: eventForm.value.phone,
+      },
       visitorPrices: this.visitorPrices,
       participantPrices: this.participantPrices,
       dates: this.dates,
-      imageUrl: this.imageUrl,
+      creator: user._id,
+      isDeleted: false,
+      isApproved: false,
     };
-    console.log('form finished', formValue);
+
+    this.eventSubscription$ = this.eventService
+      .createEvent(formValue)
+      .subscribe({
+        next: (createdEvent) => {
+          // TODO: Toaster message
+          console.log(createdEvent);
+        },
+        error: (err) => {
+          this.errorMessage = err.message;
+          console.log(err);
+        },
+      });
   }
 
   onRegionChange(region: string): void {
