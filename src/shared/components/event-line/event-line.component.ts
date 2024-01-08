d33b831bb25ddf22b07a925748c372368f6d5d79
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-01-09 00:19:43 +0200

    Create my event line

---
+++ src/shared/components/event-line/event-line.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-line',
+  templateUrl: './event-line.component.html',
+  styleUrls: ['./event-line.component.scss'],
+})
+export class EventLineComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
