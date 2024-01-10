47345bfd3fa03b4fa7bddea40802b7b673196b12
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2024-01-10 10:56:38 +0200

    make the fades responsive to 1920px. remove unused code

--- src/app/home/events-type-list/events-type-list.component.ts
+++ src/app/home/events-type-list/events-type-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { NavigationExtras, Router } from '@angular/router';
 import Categories from 'src/shared/data/categories';
 import { Category } from 'src/shared/interfaces/Category';
 
