0540922aafcfbb13f918f58879dc4497d8859f53
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2024-01-10 10:36:20 +0200

    take event categories from enum, fix sort query passed to events

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,40 +11,5 @@ export class HomePage implements OnInit {
 
   categories: Category[] = [];
 
-  ngOnInit() {
-    this.categories = [
-      {
-        type: 'Драг',
-        imageUrl: '../../assets/icon/category-icons/drag.png',
-      },
-      {
-        type: 'Дрифт',
-        imageUrl: '../../assets/icon/category-icons/drift.png',
-      },
-      {
-        type: 'Писта',
-        imageUrl: '../../assets/icon/category-icons/pista.png',
-      },
-      {
-        type: 'Планинско',
-        imageUrl: '../../assets/icon/category-icons/mountain.png',
-      },
-      {
-        type: 'Тайм Атак',
-        imageUrl: '../../assets/icon/category-icons/time-track.png',
-      },
-      {
-        type: 'Рали',
-        imageUrl: '../../assets/icon/category-icons/rally.png',
-      },
-      {
-        type: 'Мотори',
-        imageUrl: '../../assets/icon/category-icons/bikes.png',
-      },
-      {
-        type: 'Събори',
-        imageUrl: '../../assets/icon/category-icons/meetings.png',
-      },
-    ];
-  }
+  ngOnInit() {}
 }
